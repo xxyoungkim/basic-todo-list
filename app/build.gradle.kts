9fe7464c2a962e34cca3e35880f9157464259f5b
xxyoungkim <xxyoungkim@gmail.com>
2025-08-29 14:54:07 +0900

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.young.mytodo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
